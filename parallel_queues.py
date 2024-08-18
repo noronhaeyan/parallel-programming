27e7a501bc4a6eb3dd9cf708649d9c686c0f580e
Eyan Noronha <noronhaeyan@Eyans-Laptop.local>
2024-08-18 16:19:46 -0700

    single queues with times

--- parallel_queues.py
+++ parallel_queues.py
@@ -4,23 +4,33 @@ from time import sleep, time
 
 event = Event()
 
-def modify_variable(queue_in, queue_out):
+def modify_variable(queue_in: Queue, queue_out: Queue):
+    internal_t = 0
+    sleeping_t = 0
     while True:
         if not queue_in.empty():
+            t0 = time()
             var = queue_in.get()
             for i in range(len(var)):
                 var[i] += 1
             queue_out.put(var)
+            internal_t += time()-t0
+        else:
+            t0 = time()
+            sleep(0.001)
+            sleeping_t += time()-t0
         if event.is_set():
             break
-    print('Stop printing')
+    sleep(0.1)
+    print(f'Running time: {internal_t} seconds')
+    print(f'Sleeping time: {sleeping_t} seconds')
 
-my_var = [1, 2, 3]
+my_var = [3, 4, 5]
 queue1 = Queue()
 queue2 = Queue()
 queue1.put(my_var)
-t = Thread(target=modify_variable, args=(queue1, queue2))
-t2 = Thread(target=modify_variable, args=(queue2, queue1))
+t = Thread(target=modify_variable, args=(queue1, queue1))
+t2 = Thread(target=modify_variable, args=(queue1, queue1))
 t.start()
 t2.start()
 t0 = time()
